894de0a27541ad3f0dc2acbc603b3d8c3f6cf85d
Takato Hano <oh48698062welcome@gmail.com>
2022-11-03 15:17:40 +0900

    update for go mod init

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ echo "CUID=$(id -u)" > .env
 echo "CGID=$(id -g)" >> .env
 echo "CU=$(id -un)" >> .env
 echo "CG=$(id -gn)" >> .env
+echo "PRJ=$(git config --local remote.origin.url | sed -e 's/https:\/\///g' | sed -e 's/.git//g')"  >> .env
